aa7b6e17868265b450be55b46bb213de2d31e90a
Stewart Alexander <66742157+sa-fx@users.noreply.github.com>
2023-03-04 21:19:17 +0000

    Removed 3rd party libraries Added HTTP/PUSH support

---
+++ hardware/libraries/README.txt
@@ -0,0 +1,13 @@
+This code is designed for use with the following boards:
+- Arduino MEGA w WiFi shield
+OR
+- ESP32 WiFi enabled board
+
+The following libraries are required to run this code for any sensor:
+- Arduino_JSON/Arduino_JSON.h
+- LiquidCrystal_I2C/LiquidCrystal_I2C.h
+- WiFi_Link/WiFi.h
+- HTTPClient/HTTPClient.h
+
+The following libraries are required to run this code for a CO2 sensor:
+- CO2Sensor-master/CO2Sensor.h
--- hardware/libraries/sustainable_sensor/sustainable_sensor.h
+++ hardware/libraries/sustainable_sensor/sustainable_sensor.h
@@ -1,31 +1,38 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 #include <network_info.h>
+
+#define DEBUG_FLAG false
 
 #define WARNING_BUZZER 13
 
-const String sensor_status[4] = {"Unhealthy", "Not ideal", "Good", "Excellent"};
+const String sensor_status[5] = {"Dangerous", "Unhealthy", "Not ideal", "Good", "Excellent"};
 const String network_status[3] = {"Connected", "Disconnected", "Failed"};
+
+// Network constants and objects
+static IPAddress data_server(127, 0, 0, 1);
+static WiFiClient client;
+static HTTPClient http;
 
 // Set display address, and display dimensions
 static LiquidCrystal_I2C display(0x27, 20, 4);
 
-// Define the data server
-static IPAddress data_server(127, 0, 0, 1);
-static WiFiClient client;
-
 class SensorModule
 {
 public:
-  SensorModule(String display_name, String location);
+  SensorModule(String sensor_type, String display_name, String campus, String building, String room);
   void displayValues(String message);
   void setValue(int value);
   void sendData();
   void networkConnect();
 
 private:
+  String sensor_type_;
   String display_name_;
-  String location_;
+  String campus_;
+  String building_;
+  String room_;
   int sensor_value_;
 };
